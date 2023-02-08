0719cb22493767daa797a72989b09477e40cba76
Peter Ademiju <duyox2004@gmail.com>
2023-02-08 19:53:13 +0100

    changes

--- Pizza/api/__init__.py
+++ Pizza/api/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_restx import Api
 from .orders.views import order_namespace
 from .auth.views import auth_namespace
-from .config.config import config_dict
+from .config import config_dict
 from .utils import db
 from .models.orders import Order
 from .models.users import User
